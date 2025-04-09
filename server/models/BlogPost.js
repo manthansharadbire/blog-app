b878eb629aedb87bce7797e4708ab6d7fc6ea21b
Manthan Bire <manthansharadbire@gmail.com>
2025-04-09 16:56:18 +0530

    User Schema and model has been accomplished

--- server/models/BlogPost.js
+++ server/models/BlogPost.js
@@ -1,7 +1,26 @@
 import { Schema, model } from "mongoose";
 
 const blogPostSchema = new Schema ({
-
+title:{
+    type:String,
+    required:true,
+},
+content:{
+    type:String,
+    required:true,
+},
+tags:{
+    type:[String],
+},
+status:{
+    type:String,
+    enum:["Draft", "Published"],
+    default:"Draft",
+},
+createdAt:{
+    type:Date,
+},
+author
 })
 
 const BlogPost = model("BlogPost",blogPostSchema );
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,32 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-
-});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      errorMessage: "Name is required",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      errorMessage: "Email is required",
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 22,
+    },
+    city: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
