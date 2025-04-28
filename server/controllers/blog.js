e7493bb08666bedb35b991ea5afe95d93ea02266
Manthan Bire <manthansharadbire@gmail.com>
2025-04-28 10:00:27 +0530

    feat:created getPost api to fetch all the blogs

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,15 +1,52 @@
+import BlogPost from "../models/BlogPost.js";
+
+//to post or create a blog
 const postBlogs = async (req, res) => {
   const { title, content, tags } = req.body;
 
-  return res.status(201).json({
-    message: "Blog post created successfully",
-    data: {
-      title,
-      content,
-      tags,
-    },
-    success: true,
+  const author = req.user_id;
+
+  const blogPost = new BlogPost({
+    title,
+    content,
+    tags,
+    author
   });
+
+  try {
+    const savedBlogPost = await blogPost.save();
+
+    return res.status(201).json({
+      message: "Blog post created successfully",
+      data: savedBlogPost,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: `Error creating BlogPost: ${error.message}`,
+      data: null,
+      success: false,
+    });
+  }
 };
 
-export { postBlogs };
+//to fetch or retrieve blogs
+const getBlogs = async(req,res)=>{
+ const blog = await BlogPost.find({status:"published"});
+
+ if(blog.length === 0 ){
+  return res.status(404).json({
+    message:"Oops! Blog cannot be found",
+    data:null,
+    success:false
+  })
+ }
+
+ return res.status(200).json({
+  message:"Blogs fetched successfully",
+  data:blog,
+  success:true
+ })
+}
+
+export { postBlogs,getBlogs };
--- server/index.js
+++ server/index.js
@@ -4,21 +4,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import jwt from "jsonwebtoken";
-import {postBlogs} from "./controllers/blog.js"
+import { postBlogs,getBlogs } from "./controllers/blog.js";
 
 import { postSignup, postLogin } from "./controllers/user.js";
 
-const middleware = async (req, res, next) => {
-  console.log(`Request received at ${req.url}`);
-  next();
-};
-
 const app = express();
-
-//middlewares
 app.use(express.json());
 app.use(cors());
-app.use(middleware);
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -30,22 +22,15 @@ const connectDB = async () => {
   }
 };
 
-app.get("/health", (req, res) => {
-  return res.status(200).json({ message: "Server is Healthy!" });
-});
-//login
-app.post("/login", postLogin);
-//signup
-app.post("/signup", postSignup);
-
-//to fetch blogs
-app.get("/blogs", (req, res) => {
+const verifyJWT = (req, res, next) => {
   const { authorization } = req.headers;
   try {
     const jwtToken = authorization.split(" ")[1];
-    console.log(`JWT Token = ${jwtToken}`);
     const decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
     console.log("Decoded Token", decodedToken);
+
+    req.user_id = decodedToken;
+    next();
   } catch (e) {
     return res.status(401).json({
       message: `Unauthorized: ${e.message}`,
@@ -53,24 +38,24 @@ app.get("/blogs", (req, res) => {
       success: false,
     });
   }
-  const blogs = [
-    {
-      title: "Blog1",
-      content: "This is the content of Blog1",
-    },
-    {
-      title: "Blog2",
-      content: "This is the content of Blog2",
-    },
-  ];
-  return res.status(200).json({
-    message: "Blogs fetched successfully",
-    data: blogs,
-    success: true,
-  });
+};
+
+//health
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "Server is Healthy!" });
 });
 
-app.post("/blogs", postBlogs)
+//login
+app.post("/login", postLogin);
+
+//signup
+app.post("/signup", postSignup);
+
+//to fetch blogs
+app.get("/blogs", getBlogs);
+
+//to post a blog
+app.post("/blogs", verifyJWT, postBlogs);
 
 const PORT = process.env.PORT || 5002;
 
